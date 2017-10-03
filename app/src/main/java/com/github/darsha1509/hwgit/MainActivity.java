bb60a840d94dbbfe80b7fe7465ea28d33b3f0976
darya <kokorina.darya1509@gmail.com>
2017-10-03 20:27:13 +0300

    add some log to dev branch

--- app/src/main/java/com/github/darsha1509/hwgit/MainActivity.java
+++ app/src/main/java/com/github/darsha1509/hwgit/MainActivity.java
@@ -15,5 +15,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.e(LOG_TAG, "dev feature");
+
+        Log.e(LOG_TAG, "writing some log");
     }
 }
